8e1a8df9e02865e3436ceebf9e6775a9274ee1c8
Steven-Padilla <steven_padilla728@outlook.com>
2023-06-11 11:49:59 -0600

    feat: delete implemented

--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -1,9 +1,8 @@
-import { Component, Inject,inject } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { FormControl, FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Todo } from 'src/app/models/todo.model';
-
 
 @Component({
   selector: 'app-dialog',
@@ -15,7 +14,7 @@ export class DialogComponent {
     public data: { id: number; title: string; description: string },
     private matDialogRef: MatDialogRef<DialogComponent>
   ) {}
-  http=inject(HttpClient);
+  http = inject(HttpClient);
 
   todoForm = new FormGroup({
     title: new FormControl(this.data.title),
@@ -23,18 +22,35 @@ export class DialogComponent {
   });
 
   async updateTodo() {
-    
     this.http
-      .put<Todo>(`http://localhost:3000/todo/${this.data.id}`, this.todoForm.value)
+      .put<Todo>(
+        `http://localhost:3000/todo/${this.data.id}`,
+        this.todoForm.value
+      )
       .subscribe({
         next: (todoUpdated) => {
-          this.data =todoUpdated
-          this.matDialogRef.close(todoUpdated)
+          this.data = todoUpdated;
+          this.matDialogRef.close(todoUpdated);
         },
         error: (error) => {
           console.error('Request failed with error\n' + error);
         },
       });
   }
-  
+  deleteTodo() {
+    this.http
+      .delete<Todo>(`http://localhost:3000/todo/${this.data.id}`)
+      .subscribe({
+        next: () => {
+          alert(`Delete: ${this.data.id}`);
+          location.reload();
+          this.matDialogRef.close(this.data.id);
+
+        },
+        error: (error) => {
+          alert('Request failed');
+          console.error('Request failed with error\n' + error);
+        },
+      });
+  }
 }
